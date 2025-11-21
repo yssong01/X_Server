818d6c4a4bfca5098ddcf5aeb07f7dbd87d4e771
yssong <sjowun@gmail.com>
2025-11-21 14:34:35 +0900

    validator 적용

--- router/auth.mjs
+++ router/auth.mjs
@@ -1,13 +1,33 @@
 import express from "express";
 import * as authController from "../controller/auth.mjs";
+import { body } from "express-validator";
+import { validate } from "../middleware/validator.mjs";
 
 const router = express.Router();
 
+const validateLogin = [
+  body("userid")
+    .trim()
+    .isLength({ min: 4 })
+    .withMessage("최소 4자 이상 입력")
+    .matches(/^[a-zA-Z0-9]+$/)
+    .withMessage("특수문자 사용불가"),
+  body("password").trim().isLength({ min: 4 }).withMessage("최소 4자이상 입력"),
+  validate,
+];
+
+const validateSignup = [
+  ...validateLogin,
+  body("name").trim().notEmpty().withMessage("name을 입력"),
+  body("email").trim().isEmail().withMessage("이메일 형식 확인"),
+  validate,
+];
+
 // 회원 가입
-router.post("/signup", authController.signup);
+router.post("/signup", validateSignup, authController.signup);
 
 // 로그인
-router.post("/login", authController.login);
+router.post("/login", validateLogin, authController.login);
 
 // 로그인 유지
 router.post("/signup", authController.signup);
--- router/posts.mjs
+++ router/posts.mjs
@@ -1,7 +1,17 @@
 import express from "express";
 import * as postController from "../controller/post.mjs";
+import { validate } from "../middleware/validator.mjs";
+
+//설치-> C:\yssong\X\Server>npm i express-validator
+//참고 https://express-validator.github.io/docs
+import { body } from "express-validator";
 
 const router = express.Router();
+
+const validatePost = [
+  body("text").trim().isLength({ min: 4 }).withMessage("최소 4자이상 입력"),
+  validate,
+];
 
 // 전체 포스트 가져오기
 // 특정 아이디에 대한 포스트 가져오기
@@ -15,11 +25,11 @@ router.get("/:id", postController.getPost);
 
 // 포스트 쓰기
 // http://127.0.0.1:8000/post/
-router.post("/", postController.createPost);
+router.post("/", validatePost, postController.createPost);
 
 // 포스트 수정하기
 // http://127.0.0.1:8000/post/:id
-router.put("/:id", postController.updatePost);
+router.put("/:id", validatePost, postController.updatePost);
 
 // 포스트 삭제하기
 // http://127.0.0.1:8000/post/:id
